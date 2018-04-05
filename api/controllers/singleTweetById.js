c1d1101c38bcffb2372433e3448b06173fb8e2b4
siddharth <gupta.siddharth.sg@gmail.com>
2018-04-05 17:41:30 +0530

    For invalid object id

--- api/controllers/singleTweetById.js
+++ api/controllers/singleTweetById.js
@@ -1,10 +1,16 @@
 var mongoose = require('mongoose');
 var Tweet = require('../models/Tweet.js');
-
+var ObjectID = require('mongodb').ObjectID;
 module.exports.singleTweetById = function(req, res) {
-Tweet.findById(req.params.id, function(err, post) {
+	var id = req.params.id;
+	console.log(id);
+	console.log(!ObjectID.isValid(id));
+	if (!ObjectID.isValid(id)) {
+		console.log('should be here');
+		return res.send('ObjectID is invalid');
+	}
+	Tweet.findById(req.params.id, function(err, post) {
 		if (err) return next(err);
 		res.json(post);
 	});
 };
-
